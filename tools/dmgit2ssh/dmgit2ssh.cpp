ee4032602477df53a58953d224958d5f13a4432f
brinkqiang <brink.qiang@gmail.com>
2020-11-14 21:45:12 +0800

    add

--- tools/dmgit2ssh/dmgit2ssh.cpp
+++ tools/dmgit2ssh/dmgit2ssh.cpp
@@ -1,13 +1,90 @@
 
 #include "execute.h"
+#include "dmscan.h"
 
-int main( int argc, char* argv[] )
+int https2ssh(std::string strGit, Iexecute* execute)
+{
+	std::string strGitUrl;
+	fmt::scan(strGit, "https://{0}", strGitUrl);
+	strGitUrl = strGitUrl.substr(0, strGitUrl.find('/'));
+
+	if (strGitUrl.empty())
+	{
+		return -1;
+	}
+
+	std::string strName;
+	std::string strProject;
+
+
+	std::string strNameTmp = "https://" + strGitUrl + "/" + "{}";
+
+	fmt::scan(strGit, strNameTmp, strName);
+	strName = strName.substr(0, strName.find('/'));
+
+	std::string strCmd = "https://" + strGitUrl + "/" + strName + "/{}";
+	fmt::scan(strGit, strCmd, strProject);
+	strProject = strProject.substr(0, strProject.find('.'));
+
+
+	std::string strUrl = fmt::format("git remote set-url origin git@{}:{}/{}.git", strGitUrl, strName, strProject);
+
+	std::string strSSH = execute->exec(strUrl);
+
+	std::cout << fmt::format("https://{}/{}/{}.git", strGitUrl, strName, strProject) << std::endl;
+	std::cout << fmt::format("git@{}:{}/{}.git", strGitUrl, strName, strProject) << std::endl;
+
+	return 0;
+}
+
+int ssh2https(std::string strGit, Iexecute* execute)
+{
+	std::string strGitUrl;
+	fmt::scan(strGit, "git@{0}", strGitUrl);
+	strGitUrl = strGitUrl.substr(0, strGitUrl.find(':'));
+
+	if (strGitUrl.empty())
+	{
+		return -1;
+	}
+
+	std::string strName;
+	std::string strProject;
+
+	std::string strNameTmp = "git@" + strGitUrl + ":" + "{}";
+
+	fmt::scan(strGit, strNameTmp, strName);
+	strName = strName.substr(0, strName.find('/'));
+
+	std::string strCmd = "git@" + strGitUrl + ":" + strName + "/{}";
+	fmt::scan(strGit, strCmd, strProject);
+	strProject = strProject.substr(0, strProject.find('.'));
+
+	std::string strUrl = fmt::format("git remote set-url origin https://{}/{}/{}.git", strGitUrl, strName, strProject);
+
+	std::string strHTTPS = execute->exec(strUrl);
+
+	std::cout << fmt::format("https://{}/{}/{}.git", strGitUrl, strName, strProject) << std::endl;
+	std::cout << fmt::format("git@{}:{}/{}.git", strGitUrl, strName, strProject) << std::endl;
+
+	return 0;
+}
+
+int main(int argc, char* argv[])
 {
     Iexecute* execute = executeGetModule();
 
     if (NULL != execute)
     {
-        std::string strGit = execute->exec("git remote -v");
+        std::string strGit = execute->exec("git remote -v | awk \"{ print $2 }\"");
+        std::string strCompare = strGit.substr(0, strlen("https"));
+        if (strCompare.compare("https") != 0)
+        {
+			ssh2https(strGit, execute);
+            return -1;
+        }
+
+        return https2ssh(strGit, execute);
     }
 
     return 0;
